abff679b90be59a2e5fcf4d71d5dcb399c7a16a4
guanwill <w.guan@hotmail.com>
2017-07-19 00:17:06 +1000

    updated forms, added some styling

--- client/app/bundles/gameslist/components/AddGameForm.jsx
+++ client/app/bundles/gameslist/components/AddGameForm.jsx
@@ -130,18 +130,59 @@ export default class AddGameForm extends React.Component{
 
     return (
       <div>
-        <h2>{this.state.editing ? 'Update Game' : 'Add Game'}</h2>
 
-        <form onSubmit={this.handleSubmit}>
-          <input name="title" placeholder="Title" value={this.state.title} onChange={this.handleChange}/>
-          <input name="genre" placeholder="genre" value={this.state.genre} onChange={this.handleChange}/>
-          <input name="platform" placeholder="platform" value={this.state.platform} onChange={this.handleChange}/>
-          <input name="progress" placeholder="progress" value={this.state.progress} onChange={this.handleChange}/>
-          <input name="release_date" placeholder="release_date" value={this.state.release_date} onChange={this.handleChange}/>
+        {this.state.editing && (<h2>Update Game</h2>)}
+        {!this.state.editing && (<a className="game_status_title_last" data-toggle="collapse" data-target="#add_game">Add Game</a>)}
+
+
+        <form id="add_game" className={this.state.editing ? 'form-group' : 'form-group collapse'} onSubmit={this.handleSubmit}>
+          <input className="form-control add-game-field" name="title" placeholder="Title" value={this.state.title} onChange={this.handleChange}/><br/>
+
+          <select className="form-control add-game-field" placeholder="Genre" name="genre" value={this.state.genre} onChange={this.handleChange}>
+            <option value="Select"> Select Genre </option>
+            <option value="Action"> Action </option>
+            <option value="Adventure"> Adventure </option>
+            <option value="Fighting"> Fighting </option>
+            <option value="Fps"> FPS </option>
+            <option value="Sports"> Sports </option>
+            <option value="Strategy"> Strategy </option>
+            <option value="Simulation"> Simulation </option>
+            <option value="Novel"> Novel </option>
+            <option value="Other"> Other </option>
+          </select><br/>
+
+          <select className="form-control add-game-field" placeholder="Platform" name="platform" value={this.state.platform} onChange={this.handleChange}>
+            <option value="Select"> Select Platform </option>
+            <option value="PS4"> PS4 </option>
+            <option value="PS Vita "> PS Vita </option>
+            <option value="Nin 3DS "> Nin 3DS </option>
+            <option value="Nin Switch"> Nin Switch </option>
+            <option value="XBOX One"> XBOX One </option>
+            <option value="PC"> PC </option>
+            <option value="Mobile"> Mobile </option>
+            <option value="Other"> Other </option>
+          </select><br/>
+
+          <select className="form-control add-game-field" placeholder="progress" name="progress" value={this.state.progress} onChange={this.handleChange}>
+            <option value="Select"> Select Progress </option>
+            <option value="0%"> 0% </option>
+            <option value="25% "> 25% </option>
+            <option value="50% "> 50% </option>
+            <option value="75%"> 75% </option>
+            <option value="100% Storyline"> 100% Storyline </option>
+            <option value="100% Completion"> 100% Completion </option>
+            <option value="Wish"> Wish </option>
+            <option value="Contemplating"> Contemplating </option>
+            <option value="On hold"> On Hold </option>
+          </select><br/>
+
+          <input type="date" className="form-control add-game-field" name="release_date" placeholder="release_date" value={this.state.release_date} onChange={this.handleChange}/><br/>
 
           <input type="submit" value={this.state.editing ? 'Update game' : 'Add game'} className="btn btn-primary add-game-button" />
         </form>
         {this.state.editing && (<button onClick={this.deleteGame}>Delete</button>)}
+
+        {this.state.editing && (<p><Link to={ `/games` }>Back</Link></p>)}
       </div>
     )
   }
--- client/app/bundles/gameslist/components/game.jsx
+++ client/app/bundles/gameslist/components/game.jsx
@@ -1,34 +1,23 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router-dom';
-// import { formatDate } from '../utils/format';
 
 export default class Game extends React.Component {
 
-  // constructor (props) {
-  //   super(props)
-  //   this.props = {
-  //     game: props.game
-  //   }
-  // }
-  //
-  // // setting default prop
-  // static defaultProps = {
-  //   game: {}
-  // }
-  //
-  // componentDidMount () {
-  //   if(this.props.match){
-  //     $.ajax({
-  //       type: "GET",
-  //       url: `/games/${this.props.match.params.id}`,
-  //       datatype: "JSON"
-  //     }).done((data) => {
-  //       console.log('show data of this game')
-  //       console.log(data)
-  //       this.setprops({game: data});
-  //     })
-  //   }
-  // }
+  deleteGame = () => {
+    if(confirm("Are you sure?")) {
+      $.ajax({
+          type: "DELETE",
+          url: window.location.origin + `/api/gamesapi/${this.props.game.id}`,
+          })
+          .done( (data) => {
+            alert('Game deleted')
+            environment.defaultEnvironment.navigate("/games");
+          })
+          .fail( (response) => {
+            console.log('deleting failed')
+          })
+    }
+  }
 
   render () {
     return (
@@ -41,8 +30,34 @@ export default class Game extends React.Component {
 
         <td><Link to={ `/games/${this.props.game.id}/edit` }>Edit<i className="fa fa-pencil-square-o" aria-hidden="true"></i></Link> </td>
 
-        <td><a onClick={this.handleDelete}> <i className="fa fa-trash-o" aria-hidden="true"></i></a></td>
+        <td><a onClick={this.deleteGame}> <i className="fa fa-trash-o" aria-hidden="true"></i></a></td>
       </tr>
     )
   }
 }
+
+// constructor (props) {
+//   super(props)
+//   this.props = {
+//     game: props.game
+//   }
+// }
+//
+// // setting default prop
+// static defaultProps = {
+//   game: {}
+// }
+//
+// componentDidMount () {
+//   if(this.props.match){
+//     $.ajax({
+//       type: "GET",
+//       url: `/games/${this.props.match.params.id}`,
+//       datatype: "JSON"
+//     }).done((data) => {
+//       console.log('show data of this game')
+//       console.log(data)
+//       this.setprops({game: data});
+//     })
+//   }
+// }
