95162745a7a395cad849101ac11fb07d0c44a27e
guanwill <w.guan@hotmail.com>
2017-07-29 18:17:28 +1000

    styling

--- client/app/bundles/gameslist/components/gameslist/AddGameForm.jsx
+++ client/app/bundles/gameslist/components/gameslist/AddGameForm.jsx
@@ -124,59 +124,67 @@ export default class AddGameForm extends React.Component{
 
     // {this.state.editing && (<h2>Update Game</h2>)} //put title out of the form
     return (
-      <div className={this.state.editing ? 'edit-game-form' : 'add-game-form'}>
+      <div>
 
-        <form id="add_game" className={this.state.editing ? 'form-group' : 'form-group collapse'} onSubmit={this.handleSubmit}>
-          <input className="form-control add-game-field" name="title" placeholder="Title" value={this.state.title} onChange={this.handleChange}/><br/>
+        { this.state.editing && <div className="home_header_bar">
+          <Link className='gameslist_link' to={ `/games/` }>My Games List</Link>
+        </div> }
 
-          <select className="form-control add-game-field" placeholder="Genre" name="genre" value={this.state.genre} onChange={this.handleChange}>
-            <option value="Select"> Select Genre </option>
-            <option value="Action"> Action </option>
-            <option value="Adventure"> Adventure </option>
-            <option value="Fighting"> Fighting </option>
-            <option value="Fps"> FPS </option>
-            <option value="Sports"> Sports </option>
-            <option value="Strategy"> Strategy </option>
-            <option value="Simulation"> Simulation </option>
-            <option value="Novel"> Novel </option>
-            <option value="Other"> Other </option>
-          </select><br/>
-
-          <select className="form-control add-game-field" placeholder="Platform" name="platform" value={this.state.platform} onChange={this.handleChange}>
-            <option value="Select"> Select Platform </option>
-            <option value="PS4"> PS4 </option>
-            <option value="PS Vita"> PS Vita </option>
-            <option value="Nin 3DS"> Nin 3DS </option>
-            <option value="Nin Switch"> Nin Switch </option>
-            <option value="XBOX One"> XBOX One </option>
-            <option value="PC"> PC </option>
-            <option value="Mobile"> Mobile </option>
-            <option value="Other"> Other </option>
-          </select><br/>
-
-          <select className="form-control add-game-field" placeholder="progress" name="progress" value={this.state.progress} onChange={this.handleChange}>
-            <option value="Select"> Select Progress </option>
-            <option value="0%"> 0% </option>
-            <option value="25% "> 25% </option>
-            <option value="50% "> 50% </option>
-            <option value="75%"> 75% </option>
-            <option value="100% Storyline"> 100% Storyline </option>
-            <option value="100% Completion"> 100% Completion </option>
-            <option value="Wish"> Wish </option>
-            <option value="Contemplating"> Contemplating </option>
-            <option value="On hold"> On Hold </option>
-          </select><br/>
-
-          <input type="date" className="form-control add-game-field" name="release_date" placeholder="release_date" value={this.state.release_date} onChange={this.handleChange}/><br/>
-
-          <input disabled={disabledSubmitStatus} type="submit" value={this.state.editing ? 'Update Game' : 'Add Game'} className="btn btn-primary add-game-button" />
-
-          {!this.state.editing && <a className="btn btn-danger close-after-add-button"  data-toggle="collapse" data-target="#add_game">Close </a>}
-        </form>
+        <div className={this.state.editing ? 'edit-game-form' : 'add-game-form'}>
 
 
-        {this.state.editing && (<button className="btn btn-danger delete-game-button" onClick={this.deleteGame}>Delete</button>)}
-        {this.state.editing && (<p className="back-from-update-button"><Link to={ `/games` }>Back</Link></p>)}
+          <form id="add_game" className={this.state.editing ? 'form-group' : 'form-group collapse'} onSubmit={this.handleSubmit}>
+            <input className="form-control add-game-field" name="title" placeholder="Title" value={this.state.title} onChange={this.handleChange}/><br/>
+
+            <select className="form-control add-game-field" placeholder="Genre" name="genre" value={this.state.genre} onChange={this.handleChange}>
+              <option value="Select"> Select Genre </option>
+              <option value="Action"> Action </option>
+              <option value="Adventure"> Adventure </option>
+              <option value="Fighting"> Fighting </option>
+              <option value="Fps"> FPS </option>
+              <option value="Sports"> Sports </option>
+              <option value="Strategy"> Strategy </option>
+              <option value="Simulation"> Simulation </option>
+              <option value="Novel"> Novel </option>
+              <option value="Other"> Other </option>
+            </select><br/>
+
+            <select className="form-control add-game-field" placeholder="Platform" name="platform" value={this.state.platform} onChange={this.handleChange}>
+              <option value="Select"> Select Platform </option>
+              <option value="PS4"> PS4 </option>
+              <option value="PS Vita"> PS Vita </option>
+              <option value="Nin 3DS"> Nin 3DS </option>
+              <option value="Nin Switch"> Nin Switch </option>
+              <option value="XBOX One"> XBOX One </option>
+              <option value="PC"> PC </option>
+              <option value="Mobile"> Mobile </option>
+              <option value="Other"> Other </option>
+            </select><br/>
+
+            <select className="form-control add-game-field" placeholder="progress" name="progress" value={this.state.progress} onChange={this.handleChange}>
+              <option value="Select"> Select Progress </option>
+              <option value="0%"> 0% </option>
+              <option value="25% "> 25% </option>
+              <option value="50% "> 50% </option>
+              <option value="75%"> 75% </option>
+              <option value="100% Storyline"> 100% Storyline </option>
+              <option value="100% Completion"> 100% Completion </option>
+              <option value="Wish"> Wish </option>
+              <option value="Contemplating"> Contemplating </option>
+              <option value="On hold"> On Hold </option>
+            </select><br/>
+
+            <input type="date" className="form-control add-game-field" name="release_date" placeholder="release_date" value={this.state.release_date} onChange={this.handleChange}/><br/>
+
+            <input disabled={disabledSubmitStatus} type="submit" value={this.state.editing ? 'Update Game' : 'Add Game'} className="btn btn-primary add-game-button" />
+
+            {!this.state.editing && <a className="btn btn-danger close-after-add-button"  data-toggle="collapse" data-target="#add_game">Close </a>}
+          </form>
+
+
+          {this.state.editing && (<button className="btn btn-danger delete-game-button" onClick={this.deleteGame}>Delete</button>)}
+          {this.state.editing && (<p className="back-from-update-button"><Link to={ `/games` }>Back</Link></p>)}
+        </div>
       </div>
     )
   }
