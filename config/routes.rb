f4d22318a03f8f32548f4f813dfb0d61f65c8b0b
guanwill <w.guan@hotmail.com>
2017-11-18 22:24:06 +1100

    cleaned files

--- config/routes.rb
+++ config/routes.rb
@@ -8,27 +8,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/home' => 'home#index'
-  root to: "home#index"
   get '/getsearchresults/:query' => 'home#getsearchresults'
   get '/getnewgames' => 'home#getnewgames'
 
+  root to: "home#index"
+
 end
-
-
-
-# get '/games' => 'games#index'
-# resources :games do
-#   get :autocomplete_gamesapi_name, :on => :collection
-# end
-
-# post '/rate' => 'rater#create', :as => 'rate'
-# devise_for :users, :controllers => {:registrations => "my_devise/registrations"}
-# root to: "about#about"
-# root to: "games#index"
-
-
-# resources :games
-# get '/plan' => 'games#plan'
-# get '/completed' => 'games#completed'
-# get '/about' => 'about#about'
-# post '/search' => 'about#search'
---
+++ db/migrate/20171118112356_fix_users_in_games.rb
@@ -0,0 +1,4 @@
+class FixUsersInGames < ActiveRecord::Migration
+  def change
+  end
+end
