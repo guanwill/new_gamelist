6183247de1619fd5da7168c92ee908cde1a86069
guanwill <w.guan@hotmail.com>
2017-12-06 12:27:54 +1100

    refactored searchform and home component

--- client/app/bundles/gameslist/components/home/Home.jsx
+++ client/app/bundles/gameslist/components/home/Home.jsx
@@ -11,50 +11,21 @@ import { toggleSearchResults } from '../util/custom';
 
 export default class Home extends React.Component{
   static defaultProps = {
-    searchResults: [],
     currentUser: []
   }
 
   constructor(props, _railsContext){
       super(props)
       this.state = {
-        searchResults: [], // To pass data to Results component
-        currentUser: this.props.currentUser, // To pass to Results, and then ResultsItems components + Footer component
+        currentUser: this.props.currentUser, // To pass to SearchForm and Footer components
       }
-  }
-
-  // SEARCH FOR GAME FUNCTION
-  searchthis = (query) => {
-    $.ajax({
-      crossDomain: true,
-      method: 'get',
-      url: window.location.origin + '/getsearchresults/' + query,
-      success: function(response){
-        var results_count = response.results.length
-        console.log(response)
-        // Grabs results from the response and update searchResults props so we could pass it to Results Component
-        this.setState({searchResults: response.results})
-        // Depending on results_count, display relevant text to users to let them know the results
-        if (response.error == "OK"){
-          if (results_count == 0){
-            $('.divider-second-top p').text("No results")
-          }
-          else {
-            $('.divider-second-top p').text("Search Results for: " + query)
-          }
-        }
-        else {
-          $('.divider-second-top p').text("")
-        }
-      }.bind(this),
-    });
   }
 
   componentDidMount () {
     toggleSearchResults()
   }
 
-  // RENDERS SEARCH FORM, SEARCH RESULTS, AND FOOTER COMPONENTS
+  // RENDERS SEARCH FORM AND FOOTER COMPONENTS
   render() {
     return (
       <div>
@@ -62,8 +33,7 @@ export default class Home extends React.Component{
           { this.state.currentUser ? <Link className='gameslist_link' to={ `/games/` }>My Games List</Link> : <Link className='gameslist_link' to={ `/` }>My Games List</Link>  }
         </div>
 
-        <SearchForm searchthis={this.searchthis} />
-        <Results currentUser={this.state.currentUser} searchResults={this.state.searchResults}/>
+        <SearchForm currentUser={this.state.currentUser} />
         <Footer />
         <FooterBottom currentUser={this.state.currentUser} />
       </div>
--- client/app/bundles/gameslist/components/home/SearchForm.jsx
+++ client/app/bundles/gameslist/components/home/SearchForm.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Results from './Results';
+
 
 export default class SearchForm extends React.Component{
+  static defaultProps = {
+    searchResults: [],
+  }
+
+  constructor(props, _railsContext){
+    super(props)
+    this.state = {
+      searchResults: [], // To pass data to Results child component
+      currentUser: this.props.currentUser, // Grab currentuser props from parent component pass this to the Results child component
+    }
+  }
+
+  // Search function
+  searchGame = (query) => {
+    $.ajax({
+      crossDomain: true,
+      method: 'get',
+      url: window.location.origin + '/getsearchresults/' + query,
+      success: function(response){
+        var results_count = response.results.length
+        console.log(response)
+        // Grabs results from the response and update searchResults props so we could pass it to Results child component
+        this.setState({searchResults: response.results})
+        // Depending on results_count, display relevant text to users to let them know the results
+        if (response.error == "OK"){
+          if (results_count == 0){
+            $('.divider-second-top p').text("No results")
+          }
+          else {
+            $('.divider-second-top p').text("Search Results for: " + query)
+          }
+        }
+        else {
+          $('.divider-second-top p').text("")
+        }
+      }.bind(this),
+    });
+  }
 
   // Grab user input data
-  createAjax = (e) => {
+  initiateSearch = (e) => {
     e.preventDefault()
     if (this.titleInput.value.trim() == "") {
       var query = "Nothing"
@@ -13,23 +53,28 @@ export default class SearchForm extends React.Component{
       var query = this.titleInput.value
     }
 
-    this.props.searchthis(query)
+    // Initiate Search
+    this.searchGame(query)
     console.log(query)
     $('.divider-second-top p').text("Searching...")
   }
 
   render() {
     return (
-      <div className="search-div">
-          <div className="welcome-div">
-            <div className="api_form">
-              <form className='form-group search-game-form' onSubmit={this.createAjax}>
-                <input type="text" ref={(input) => {this.titleInput = input}} className="ui-autocomplete-input gamename-input" placeholder="Search for a game"/>
-                <button type="button" onClick={this.createAjax} className="search-game-button btn-primary" ><i className="fa fa-search" aria-hidden="true"></i></button>
-              </form>
-            </div>
+      <div>
+        <div className="search-div">
+            <div className="welcome-div">
+              <div className="api_form">
+                <form className='form-group search-game-form' onSubmit={this.initiateSearch}>
+                  <input type="text" ref={(input) => {this.titleInput = input}} className="ui-autocomplete-input gamename-input" placeholder="Search for a game"/>
+                  <button type="button" onClick={this.initiateSearch} className="search-game-button btn-primary" ><i className="fa fa-search" aria-hidden="true"></i></button>
+                </form>
+              </div>
 
-          </div>
+            </div>
+        </div>
+
+        <Results currentUser={this.state.currentUser} searchResults={this.state.searchResults}/>
       </div>
     )
   }
